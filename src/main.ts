ef52483da2f29cd05ab4594404d9866bf33df5b9
spring500 <zengquanrun@kurogame.com>
2023-12-01 18:07:10 +0800

    调整窗口最小值

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { app, BrowserWindow } from 'electron';
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800, height: 600,
+        minWidth: 700, minHeight: 200,
         alwaysOnTop: true,
         autoHideMenuBar: true,
         frame: false,
