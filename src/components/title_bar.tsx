903f866be8195d3d5f5c0b205412fbab88715579
spring500 <zengquanrun@kurogame.com>
2023-12-08 11:32:56 +0800

    change title

--- src/components/title_bar.tsx
+++ src/components/title_bar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Component_TitleBar() {
     return <div className='titleBar'>
-        <div className='titleBarText'>AizenLogWatcher</div>
+        <div className='titleBarText'>AizenTail</div>
         <button className='titleBarButton' id="minimizeButton" onClick={() => (window as any).electron.windowMinimize()}>▁</button>
         <button className='titleBarButton' id="maximizeButton" onClick={() => (window as any).electron.windowMaximize()}>▢</button>
         <button className='titleBarButton' id="closeButton" onClick={() => window.close()}>╳</button>
