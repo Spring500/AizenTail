571e18676f33d2633db334a0e7d7d4b083ca17e6
spring500 <zengquanrun@kurogame.com>
2024-05-30 10:27:45 +0800

    删除无用类型定义，预备合并染色及筛选规则

--- src/renderer/src/log_types.ts
+++ src/renderer/src/log_types.ts
@@ -1,27 +1,22 @@
 type ColorConfig = {
-    color?: string,
-    background?: string,
-    reg: string,
-    enable?: boolean,
-    regexEnable?: boolean,
+    color?: string
+    background?: string
+    reg: string
+    enable?: boolean
+    exclude?: boolean
+    regexEnable?: boolean
 }
 
 type ReplaceConfig = {
-    reg: string,
-    replace: string,
-    enable?: boolean,
-    regexEnable?: boolean,
+    reg: string
+    replace: string
+    enable?: boolean
+    regexEnable?: boolean
 }
 
 type FilterConfig = {
-    reg: string,
-    exclude: boolean,
-    enable?: boolean,
-    regexEnable?: boolean,
-}
-
-type LogConfig = {
-    colorRules: ColorConfig[],
-    replaceRules: ReplaceConfig[],
-    filterRules: FilterConfig[],
+    reg: string
+    exclude: boolean
+    enable?: boolean
+    regexEnable?: boolean
 }
