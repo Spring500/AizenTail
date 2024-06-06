0f149286c1f0df468c317b36922b46f131319eee
spring500 <zengquanrun@kurogame.com>
2024-06-06 18:37:22 +0800

    开发环境默认打开dev tool

--- src/main/index.ts
+++ src/main/index.ts
@@ -25,6 +25,7 @@ const createWindow = (): void => {
     // HMR for renderer base on electron-vite cli.
     // Load the remote URL for development or the local html file for production.
     if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+        mainWindow.webContents.openDevTools({ mode: 'detach' })
         mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
     } else {
         mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
