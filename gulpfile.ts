026f284d1704aface048d8e15e07d08d5f15638a
spring500 <zengquanrun@kurogame.com>
2024-08-15 10:04:33 +0800

    导出pdb

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,7 +4,7 @@ import { src, dest } from 'gulp'
 export function copyAddon(cb) {
     // return cb()
     console.log('迁移 node-addon-api/build/release/**')
-    return src('node-addon-api/build/Release/**/*.node', {
+    return src('node-addon-api/build/Release/**/*.{node,pdb}', {
         removeBOM: false,
         encoding: false
     }).pipe(dest('build/Release', {}))
